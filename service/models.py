f85865ceb0c601b5ebb5ba7bce29d9568a2cbf86
View <xinyu9914@gmail.com>
2022-12-11 13:47:20 +0800

    init

---
+++ service/models.py
@@ -0,0 +1,40 @@
+from django.contrib.gis.db import models
+from django.contrib.auth import get_user_model
+
+
+# Create your models here.
+class WorldBorder(models.Model):
+    # Regular Django fields corresponding to the attributes in the
+    # world borders shapefile.
+    name = models.CharField(max_length=50)
+    area = models.IntegerField()
+    pop2005 = models.IntegerField('Population 2005')
+    fips = models.CharField('FIPS Code', max_length=2, null=True)
+    iso2 = models.CharField('2 Digit ISO', max_length=2)
+    iso3 = models.CharField('3 Digit ISO', max_length=3)
+    un = models.IntegerField('United Nations Code')
+    region = models.IntegerField('Region Code')
+    subregion = models.IntegerField('Sub-Region Code')
+    lon = models.FloatField()
+    lat = models.FloatField()
+
+    # GeoDjango-specific: a geometry field (MultiPolygonField)
+    mpoly = models.MultiPolygonField()
+
+    # Returns the string representation of the model.
+    def __str__(self):
+        return self.name
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    modified = models.DateTimeField(auto_now=True, editable=False)
+    last_location = models.PointField(
+        editable=False,
+        blank=True,
+        null=True,
+        default=None)
+
+    def __str__(self):
+        return f"{self.user}"
